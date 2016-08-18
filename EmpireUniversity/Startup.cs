460882df16030e92ac289dce648f2185aa97f469
GolamSaroarGitHub <golamsaroar89@gmail.com>
2016-08-18 11:04:11 +0600

    The initial stage of the university project

---
+++ EmpireUniversity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmpireUniversity.Startup))]
+namespace EmpireUniversity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
